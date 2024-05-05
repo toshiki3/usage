ead6af1daf0bd23c74e721e3c7b352eaf986f8a5
toshiki3 <sjungui457@gmail.com>
2024-05-05 14:15:10 +0900

    文言追加(変更説明)

--- psutil1.py
+++ psutil1.py
@@ -2,3 +2,4 @@ import psutil
 print(f'CPU :{psutil.cpu_percent(interval=1):5}%')
 print(f'Memory:{psutil.virtual_memory().percent:5}%')
 print(f"Disk:{psutil.disk_usage('/').percent:5}%")
+print("各使用率は以上の通りです")
